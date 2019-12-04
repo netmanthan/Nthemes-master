ba0a0f8fc681c75448fb8d139d5a99dba1d31651
Jawahar Ramkripal Mallah <jawahar.mallah@netmanthan.com>
2019-12-04 12:05:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in Nthemes/__init__.py
-from bluetheme import __version__ as version
+from Nthemes import __version__ as version
 
 setup(
 	name='Nthemes',
